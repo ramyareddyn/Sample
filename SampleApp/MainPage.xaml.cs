38d0dce9d63e4833bada1bcca1a150adeffbc6c1
Ramya <ramya.n@winwire.com>
2018-07-05 17:39:30 +0530

    Initial check-in of SampleApp

---
+++ SampleApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel mainPageViewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            mainPageViewModel = new MainPageViewModel(this);
+            BindingContext = mainPageViewModel;
+        }
+    }
+}
---
+++ SampleApp/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        MainPage mainPage;
+        public MainPageViewModel(MainPage mainPageRef)
+        {
+            mainPage = mainPageRef;
+        }
+
+        private Command onTapped;
+        private const string OnTappedCommandPropertyName = "OnTapped";
+
+        public Command OnTapped
+        {
+            get
+            {
+                return onTapped ?? (onTapped = new Command(ExecuteOnTappedCommand));
+            }
+        }
+
+        public void ExecuteOnTappedCommand()
+        {
+            //your code here
+            mainPage.DisplayAlert("XamEffects", "Tapped", "OK");
+        }
+    }
+}
